6185a5eeee0b0a8cf9193b50d8a8bfc80a2d5355
Danny Ng <danny@Dannys-MBP.fios-router.home>
2022-12-02 14:14:11 -0500

    added tests

--- Makefile
+++ Makefile
@@ -9,5 +9,11 @@ network.o: network.cpp network.h
 
 social3.o: social3.cpp social3.h
 
+tests: tests.o Profile.o network.o social3.o
+	g++ -o tests tests.o Profile.o network.o social3.o
+
+tests.o: tests.cpp Profile.h network.h social3.h
+	g++ -c -std=c++11 tests.cpp
+
 clean:
-	rm -f main.o Profile.o n
+	rm -f main tests main.o tests.o Profile.o network.o social3.o
---
+++ tests.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "Profile.h"
+#include "network.h"
+
+TEST_CASE("TASK A"){
+    Profile p1("marco", "Marco"); 
+    CHECK((p1.getUsername() == "marco" && p1.getFullName() == "Marco (@marco)"));
+
+    p1.setDisplayName("Marco Rossi"); 
+    CHECK((p1.getUsername() == "marco" && p1.getFullName() == "Marco Rossi (@marco)"));
+}
+
+TEST_CASE("TASK B"){
+    Network nw;
+    CHECK(nw.addUser("mario", "Mario") == true);
+    CHECK(nw.addUser("mario", "Mario2") == false);
+}
